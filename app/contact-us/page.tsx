731e76b9031de11a471b1e7fc48bfbee3617a2bc
lojith <lojithvinsuka1998@gmail.com>
2023-09-12 19:00:51 +0530

    color change

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -24,22 +24,22 @@ const ContactUs = (props: Props) => {
             <div className="flex h-full w-1/2 flex-col items-start justify-center px-10">
                 <div className={`font-bold ${dmSerifDisplay.className} text-[48px] text-[#2da74b]`}>Contact Us</div>
                 <div className={`${bitter.className} mt-10`}>
-                    <div className='flex font-bold gap-2'><MapPin />Address:</div>
+                    <div className='flex font-bold gap-2 text-[#2da74b]'><MapPin />Address:</div>
                     <div>
                         &quot;Priyankara Tea Buyer Company&quot; <br />
                         Ketandola,<br />
                         Thalagaspe,<br />
                         Elpitiya
                     </div>
-                    <div className='flex font-bold mt-5 gap-2'><UserCircle2 />Owner:</div>
+                    <div className='flex font-bold mt-5 gap-2 text-[#2da74b]'><UserCircle2 />Owner:</div>
                     <div>
                         K.W. Ajith Priyankara
                     </div>
-                    <div className='flex font-bold mt-5 gap-2'><Phone />Contact:</div>
+                    <div className='flex font-bold mt-5 gap-2 text-[#2da74b]'><Phone />Contact:</div>
                     <div>
                         091 2254301
                     </div>
-                    <div className='flex font-bold mt-5 gap-2'><Mail />Email:</div>
+                    <div className='flex font-bold mt-5 gap-2 text-[#2da74b]'><Mail />Email:</div>
                     <div>
                         priyatea33@gmail.com
                     </div>
