243d4282c42cee58717e28bdab36559be4b9a00f
lojith <lojithvinsuka1998@gmail.com>
2023-09-12 19:02:50 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <div className='w-1/2 h-full flex justify-center items-center flex-col'>
         <div className={`font-bold ${dmSerifDisplay.className} text-[58px] bg-white p-10 text-[#2da74b] `}>Welcome to Home of
           <br /> Priyankara Tea Buyers</div>
-        <div className={`${dmSerifDisplay.className} text-[32px] text-[#07393cff]`}>-Steeped in Downsouth Elegance-</div>
+        <div className={`${dmSerifDisplay.className} text-[32px] text-[#296b6e]`}>-Steeped in Downsouth Elegance-</div>
         <div className={`p-10 ${bitter.className} text-center text-[#07393cff]`}>We, the best tea leaf collectors in Downsouth who collects tea leaf from the small tea planters in your villages.</div>
       </div>
       <div className="w-1/2 h-full  bg-[url('/tea-bg.jpg')] bg-cover"></div>
