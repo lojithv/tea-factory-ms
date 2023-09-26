00a438be15efc07ce1ed60c2a6a48466b96ff7af
Gavindu Rathnayaka <rathnayakagavindu98@gmail.com>
2023-09-26 18:15:18 +0530

    customer page integrated

--- app/Customers/page.tsx
+++ app/Customers/page.tsx
@@ -1,5 +1,8 @@
+"use client"
+import UserCard from '@/components/UserCard'
 import { Bitter, DM_Serif_Display } from 'next/font/google'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 type Props = {}
 
@@ -15,16 +18,44 @@ const bitter = Bitter({
 })
 
 const page = (props: Props) => {
+    const [customers, setCustomers] = useState<any>();
+    const [error, setError] = useState<any>(null);
+    const supabase = createClientComponentClient()
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const { data, error } = await supabase
+                    .from('users')
+                    .select('*')
+                    .eq('usertype', 'customer');
+
+                if (error) {
+                    setError(error);
+                } else {
+                    setCustomers(data);
+                }
+            } catch (error) {
+                setError(error);
+            }
+        }
+
+        fetchData();
+    }, []);
     return (
-        <div className='flex flex-grow items-center'>
-            <div className="flex h-full w-full flex-col items-center justify-start px-10">
-                <div className={`font-bold ${dmSerifDisplay.className} text-[48px] text-[#2da74b]`}>Customers</div>
-                <div className='flex'>
-
+        <>
+            <div className="flex mb-4">
+                <div className="w-1/4  h-auto"></div>
+                <div className="w-2/4  h-auto flex flex-col items-center">
+                    <div className={`font-bold ${dmSerifDisplay.className} text-[48px] text-[#2da74b] mb-2`}>Customers</div>
+                    
+                    {customers?.map((user: any, index: any) => (
+                        <UserCard index={index} user={user} />
+                    ))}
                 </div>
-
+                <div className="w-1/4  h-auto"></div>
             </div>
-        </div>
+        </>
     )
 }
 
