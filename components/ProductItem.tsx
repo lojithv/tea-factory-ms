eed179c002e5f3592613ec51ab929d7437a0b016
lojith <lojithvinsuka1998@gmail.com>
2023-10-16 19:43:44 +0530

    changes

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -30,7 +30,24 @@ const ProductItem = (props: Props) => {
     };
 
     const sendDeleteData = async () => {
-        props.onDeleteProduct(props.id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                props.onDeleteProduct(props.id);
+                // Swal.fire(
+                //     'Deleted!',
+                //     'Your file has been deleted.',
+                //     'success'
+                // )
+            }
+        })
     }
 
     return (
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -39,8 +39,21 @@ const UserCard: React.FC<UserProps> = ({ user, index, isDelete, onDeleteUser }) 
         }
     }
     const sendDeleteUserId = (userId: any) => {
-        if (onDeleteUser)
-            onDeleteUser(userId);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                if (onDeleteUser)
+                    onDeleteUser(userId);
+            }
+        })
+
     };
     const togglePopup = () => {
         setIsOpen(!isOpen);
