db5d51e8742f6deb8149d088c888573b7513cc4f
lojith <lojithvinsuka1998@gmail.com>
2023-10-18 19:50:27 +0530

    changes

--- app/supplier-history/page.tsx
+++ app/supplier-history/page.tsx
@@ -122,7 +122,7 @@ const SupplierHistory = (props: Props) => {
                                 value={endDate}
                                 onChange={(newValue) => setEndDate(newValue)}
                             />
-                            <div onClick={filterByDateRange}>Apply</div>
+                            <div className='bg-[#2da74b] text-white hover:bg-[#255e33] p-2' onClick={filterByDateRange}>Apply</div>
                         </div>
                     </LocalizationProvider>
                     {!showSearchResults && supplyHistory?.map((item, i) => {
