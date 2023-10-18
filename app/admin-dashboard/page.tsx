8439e5ef8fafd7d5f11e79094b506e126ddf814a
lojith <lojithvinsuka1998@gmail.com>
2023-10-18 11:27:38 +0530

    changes

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -8,7 +8,16 @@ type Props = {}
 const AdminDashboard = (props: Props) => {
     return (
         <div>
+            <div>Best Suppliers of Last 30 Days</div>
             <BestSupplierChart />
+
+            <div>Best Customers of Last 30 Days</div>
+
+            <div>Monthly supplied tea leaf details</div>
+
+            <div>Weekly supplied tea leaf details</div>
+
+            <div>Monthly order details</div>
         </div>
     )
 }
