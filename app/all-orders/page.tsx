cfd23956692375442d3b28fb5a9eadf77a2c8c03
lojith <lojithvinsuka1998@gmail.com>
2023-10-18 10:26:09 +0530

    changes

--- app/all-orders/page.tsx
+++ app/all-orders/page.tsx
@@ -4,6 +4,9 @@ import { Bitter, DM_Serif_Display } from 'next/font/google'
 import useUser from '@/hooks/useUser'
 import React, { useState, useEffect } from 'react'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs, { Dayjs } from 'dayjs'
 
 type Props = {}
 
@@ -23,6 +26,9 @@ const AllOrders = (props: Props) => {
     const [error, setError] = useState<any>(null);
     const user = useUser()
 
+    const [startDate, setStartDate] = React.useState<Dayjs | null>(dayjs());
+    const [endDate, setEndDate] = React.useState<Dayjs | null>(dayjs());
+
     useEffect(() => {
         if (!orders.length) {
             supabase.from('orders').select('*, users(*)').eq('status', 'pending').then((res) => {
@@ -34,12 +40,36 @@ const AllOrders = (props: Props) => {
         }
     }, [])
 
+    const filterByDateRange = async () => {
+        const { data, error } = await supabase.from('users').select('*, users(*)').lt('created_at', endDate).gte('created_at', startDate)
+        console.log(data)
+        if (data) {
+
+        }
+    }
+
     return (
         <>
             <div className="flex mb-4">
                 <div className="w-1/4  h-auto"></div>
                 <div className="w-2/4  h-auto flex flex-col items-center">
                     <div className={`font-bold ${dmSerifDisplay.className} text-[48px] text-[#2da74b] mb-2`}>Orders</div>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <div className='flex gap-3 items-center mb-4'>
+                            <DatePicker
+                                label="Start Date"
+                                value={startDate}
+                                onChange={(newValue) => setStartDate(newValue)}
+                            />
+                            <div>-</div>
+                            <DatePicker
+                                label="End Date"
+                                value={endDate}
+                                onChange={(newValue) => setEndDate(newValue)}
+                            />
+                            <div>Apply</div>
+                        </div>
+                    </LocalizationProvider>
                     {orders.map((item: any, index: any) => (
                         <OrderedCard order={item} index={index} key={index} />
                     ))}
---
+++ components/MyOrderedCard.tsx
@@ -0,0 +1,125 @@
+"use client"
+
+import { Bitter, DM_Serif_Display } from 'next/font/google'
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react'
+import moment from 'moment';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
+const dmSerifDisplay = DM_Serif_Display({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+const bitter = Bitter({
+    subsets: ['latin'],
+    weight: '400'
+})
+interface UserProps {
+    order: any;
+    index: number;
+}
+const MyOrderedCard: React.FC<UserProps> = ({ order, index }) => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [items, setItems] = useState([] as any[])
+    // const router = useRouter();
+
+    const [orderData, setOrderData] = useState(order)
+
+    const supabase = createClientComponentClient()
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+    useEffect(() => {
+
+        const parsedItems = JSON.parse(order?.items);
+        setItems(parsedItems)
+    }, [order])
+
+    const formattedDate = moment(order.created_at).format('MMMM Do YYYY');
+
+    const handleAcceptReject = async (action: string) => {
+        const { data, error } = await supabase.from('orders').update({ status: action }).eq('id', order.id)
+        setOrderData({ ...order, status: action })
+        console.log(data)
+        console.log(error)
+    }
+
+    return (
+        <div key={index} className={` w-full p-2 mb-1 mt-1 bg-white border border-gray-200 rounded-lg shadow sm:p-4 `}>
+            <div className="flow-root">
+
+                <table className="table-fixed min-w-full">
+                    <thead>
+                        <tr>
+                            <td className='w-2/4'>
+                                <p className={`${dmSerifDisplay.className} text-sm font-medium text-gray-900 truncate `}>
+                                    Items
+                                </p>
+                            </td>
+                            <td className='w-1/4'>
+                                <p className={`${dmSerifDisplay.className} text-sm font-medium text-gray-900 truncate `}>
+                                    Quantity
+                                </p>
+                            </td>
+                            <td className='w-1/4'>
+                                <p className={`${dmSerifDisplay.className} text-sm font-medium text-gray-900 truncate `}>
+                                    Price
+                                </p>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items?.map((item: any, index: number) => (
+                            <tr key={index}>
+                                <td >
+                                    <p className={`${dmSerifDisplay.className} text-sm text-green-700 truncate `}>
+                                        {item?.product || item?.name}
+                                    </p>
+                                </td>
+                                <td>
+                                    <p className={`${dmSerifDisplay.className} text-sm text-green-700 truncate `}>
+                                        {item?.qty}
+                                    </p>
+                                </td>
+                                <td>
+                                    <p className={`${dmSerifDisplay.className} text-sm text-green-700 truncate `}>
+                                        Rs. {item?.price}
+                                    </p>
+                                </td>
+                            </tr>
+                        ))}
+                        <tr>
+                            <td >
+                                <p className={`${dmSerifDisplay.className} mt-4 text-sm font-medium text-gray-900 truncate `}>
+                                    Total
+                                </p>
+                            </td>
+                            <td></td>
+                            <td>
+                                <p className={`${dmSerifDisplay.className} text-sm font-medium text-gray-900 truncate `}>
+                                    Rs. {order.total}
+                                </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='flex justify-between items-center mt-4 '>
+                <p className={`${dmSerifDisplay.className} text-sm text-center font-medium text-green-700 truncate `}>
+                    Ordered Date: {formattedDate}
+                </p>
+
+
+                <div className='flex gap-2'>
+                    {orderData.status}
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default MyOrderedCard;
